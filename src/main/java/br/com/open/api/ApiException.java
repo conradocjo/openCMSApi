39e87f15ad7437ab1fb766e10f2dc32797d4bd9c
conrado <conradocjo@gmail.com>
2020-08-25 19:55:16 -0300

    Evolução da arquitetura

--- src/main/java/br/com/open/api/ApiException.java
+++ src/main/java/br/com/open/api/ApiException.java
@@ -2,10 +2,12 @@ package br.com.open.api;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-08-24T23:05:39.296Z")
 
-public class ApiException extends Exception{
-    private int code;
-    public ApiException (int code, String msg) {
-        super(msg);
-        this.code = code;
-    }
+public class ApiException extends Exception {
+	private int code;
+
+	public ApiException(int code, String msg) {
+		super(msg);
+		this.code = code;
+	}
+
 }
